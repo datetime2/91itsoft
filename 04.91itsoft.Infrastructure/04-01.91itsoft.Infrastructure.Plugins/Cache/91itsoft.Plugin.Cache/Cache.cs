380bf51cb504b05746e8e556aef6a52acd861576
datetime2 <kozo.wo1aspx@gmail.com>
2017-04-19 18:29:23 +0800

    ,

---
+++ 04.91itsoft.Infrastructure/04-01.91itsoft.Infrastructure.Plugins/Cache/91itsoft.Plugin.Cache/Cache.cs
@@ -0,0 +1,93 @@
+﻿using Autofac;
+using Autofac.Builder;
+using Autofac.Configuration;
+using System;
+
+namespace _91itsoft.Plugin.Cache
+{
+    public class Cache
+    {
+        private static ICache _cache = null;
+        private static readonly object _cacheLocker = new object();
+        static Cache()
+        {
+            Load();
+        }
+        public static T Get<T>(string key)
+        {
+            return string.IsNullOrWhiteSpace(key) ? default(T) : _cache.Get<T>(key);
+        }
+        public static void Set(string key, object value, int minutes)
+        {
+            if (!string.IsNullOrWhiteSpace(key) && (value != null) && minutes>0)
+            {
+                lock (_cacheLocker)
+                {
+                    _cache.Set(key, value, minutes);
+                }
+            }
+        }
+        public static void Set(string key, object value, int minutes, bool isAbsoluteExpiration,
+            Action<string, object, string> onRemove)
+        {
+            if (!string.IsNullOrWhiteSpace(key) && (value != null) && minutes > 0)
+            {
+                lock (_cacheLocker)
+                {
+                    _cache.Set(key, value, minutes,isAbsoluteExpiration,onRemove);
+                }
+            }
+        }
+        public static bool IsSet(string key)
+        {
+            return _cache.IsSet(key);
+        }
+        public static void Remove(string key)
+        {
+            if (!string.IsNullOrWhiteSpace(key))
+            {
+                lock (_cacheLocker)
+                {
+                    _cache.Remove(key);
+                }
+            }
+        }
+        public static void RemoveByPattern(string pattern)
+        {
+            if (!string.IsNullOrWhiteSpace(pattern))
+            {
+                lock (_cacheLocker)
+                {
+                    _cache.RemoveByPattern(pattern);
+                }
+            }
+        }
+        public static void ClearAll()
+        {
+            _cache.ClearAll();
+        }
+        private static void Load()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ICache>();
+            builder.RegisterModule(new ConfigurationSettingsReader("autofac"));
+            IContainer context = null;
+            try
+            {
+                context = builder.Build(ContainerBuildOptions.None);
+                _cache = context.Resolve<ICache>();
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("注册缓存服务失败", exception);
+            }
+            finally
+            {
+                if (context != null)
+                {
+                    context.Dispose();
+                }
+            }
+        }
+    }
+}
