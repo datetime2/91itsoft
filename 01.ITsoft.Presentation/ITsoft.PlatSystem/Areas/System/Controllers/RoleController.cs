d7f4356c8b667ff94fb107eadb8ab9f364338001
datetime2 <kozo.wo1aspx@gmail.com>
2017-05-03 13:39:42 +0800

    ,

--- 01.ITsoft.Presentation/ITsoft.PlatSystem/Areas/System/Controllers/RoleController.cs
+++ 01.ITsoft.Presentation/ITsoft.PlatSystem/Areas/System/Controllers/RoleController.cs
@@ -48,6 +48,13 @@ namespace ITsoft.PlatSystem.Areas.System.Controllers
             var data = _roleServiceApp.FindBy(keyValue);
             return Json(data, JsonRequestBehavior.AllowGet);
         }
+        /// <summary>
+        /// 角色修改(权限修改)
+        /// </summary>
+        /// <param name="roleEntity"></param>
+        /// <param name="menuIds"></param>
+        /// <param name="keyValue"></param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult submitForm(RoleDTO roleEntity, string menuIds,Guid? keyValue)
         {
@@ -55,11 +62,27 @@ namespace ITsoft.PlatSystem.Areas.System.Controllers
             _roleServiceApp.UpdateRoleMenu(roleEntity, menuIds.Split(',').Select(s => Guid.Parse(s)));
             return Success("操作成功");
         }
+        /// <summary>
+        /// 获取角色权限
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <returns></returns>
         [HttpGet]
         public JsonResult roleAuthorize(Guid? roleId)
         {
             var grid = _roleServiceApp.RoleModuleTree(roleId.HasValue ? roleId.Value : Guid.Empty);
             return Json(grid, JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="keyValue"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult deleteForm(Guid keyValue)
+        {
+            _roleServiceApp.Remove(keyValue);
+            return Success("操作成功");
         }
     }
 }
